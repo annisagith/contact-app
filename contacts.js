dfa71e8ffafc00c4a70f96a6711786c807cc6637
annisagith <ag.work.annisamarfadilla@gmail.com>
2026-06-15 20:49:30 +0700

    add: validasi dan manipulasi command

--- contacts.js
+++ contacts.js
@@ -1,11 +1,7 @@
 const { resolve } = require("dns");
 const fs = require("fs");
-
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const chalk = require("chalk");
+const validator = require("validator");
 
 // membuat folder data
 const dirPath = "./data";
@@ -19,27 +15,41 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
 }
 
-// membungkus setiap pertanyaan ke dalam promises
-const tulisPertanyaan = (pertanyaan) => {
-  return new Promise((resolve, reject) => {
-    rl.question(pertanyaan, (nama) => {
-      resolve(nama);
-    });
-  });
-};
-
 const simpanContact = (nama, email, noHP) => {
   const contact = { nama, email, noHP };
 
   const file = fs.readFileSync("data/contacts.json", "utf-8");
   const contacts = JSON.parse(file);
 
+  // CEK DUPLIKAT
+
+  const duplikat = contacts.find((contact) => contact.nama == nama);
+  if (duplikat) {
+    console.log(
+      chalk.red.inverse.bold("Contact sudah terdaftar, gunakan nama lain"),
+    );
+    return false;
+  }
+
+  // cek email
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.inverse.bold("Email tidak valid"));
+      return false;
+    }
+  }
+
+  // cek nomor hp
+  if (!validator.isMobilePhone(noHP, "id-ID")) {
+    console.log(chalk.red.inverse.bold("Nomor HP tidak valid"));
+    return false;
+  }
+
   contacts.push(contact);
 
   fs.writeFileSync("data/contacts.json", JSON.stringify(contacts, null, 2));
 
-  console.log(`Terimakasih sudah memasukkan data.`);
-  rl.close();
+  console.log(chalk.green.inverse.bold(`Terimakasih sudah memasukkan data.`));
 };
 
-module.exports = { tulisPertanyaan, simpanContact };
+module.exports = { simpanContact };
